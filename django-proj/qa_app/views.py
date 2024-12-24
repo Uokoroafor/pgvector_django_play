9081c243c914df61037a5174221f2e74c0218165
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-12-24 19:08:35 +0000

    Adding ability to add elements to the database

--- django-proj/qa_app/views.py
+++ django-proj/qa_app/views.py
@@ -2,22 +2,39 @@ from django.shortcuts import render
 from .models import LangchainPgEmbedding
 from .embedding import get_embedding
 from pgvector.django import CosineDistance
+from uuid import uuid4
 
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
-        text = request.POST.get("input_text")
-        # Create Embedding of text
-        embedded_text = get_embedding(text)
-        document = LangchainPgEmbedding.objects.order_by(
-            CosineDistance("embedding", embedded_text)
-        ).first()
-        context = {
-            "text": text,
-            "most_similar": document,
-        }
-        return render(request, "results.html", context)
+        if "index_search" in request.POST:
+            text = request.POST.get("input_text")
+            # Create Embedding of text
+            embedded_text = get_embedding(text)
+            document = LangchainPgEmbedding.objects.order_by(
+                CosineDistance("embedding", embedded_text)
+            ).first()
+            context = {
+                "text": text,
+                "most_similar": document,
+            }
+            return render(request, "results.html", context)
+        elif "update_db" in request.POST:
+            text = request.POST.get("input_text")
+            # Create Embedding of text
+            embedded_text = get_embedding(text)
+            db_entry = LangchainPgEmbedding.objects.create(
+                embedding=embedded_text,
+                document=text,
+                uuid=uuid4(),
+            )
+            context = {
+                "text": text,
+                "embedding": embedded_text,
+                "primary_key": db_entry.pk,
+            }
+            return render(request, "add_entry.html", context)
 
     embeddings = LangchainPgEmbedding.objects.all()
     context = {"embeddings": embeddings}
