efd9dd32b1b3b33bf2968613bc66ddabc17b899f
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-12-31 05:47:38 +0000

    Add changes to load_texts.py

--- load_texts.py
+++ load_texts.py
@@ -4,13 +4,19 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from decouple import config
 import argparse
+import os
+import getpass
 
 
-OPENAI_API_KEY = config("OPENAI_API_KEY")
-if not OPENAI_API_KEY:
-    OPENAI_API_KEY = input("Please Provide OPENAI API KEY:").strip()
-    if not OPENAI_API_KEY:
-        raise ValueError("Must Provide OPENAI API KEY")
+OPENAI_API_KEY = config("OPENAI_API_KEY", default="")
+if OPENAI_API_KEY == "":
+    try:
+        OPENAI_API_KEY = getpass.getpass("Please Provide OPENAI API KEY:")
+        os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+    except Exception as e:
+        raise ValueError(
+            f"Please Provide OPENAI API KEY in the .env file and rerun. Encountered error {e} when trying to set the variable"
+        )
 
 db_user = config("DB_USER")
 db_password = config("DB_PASSWORD")
@@ -23,7 +29,11 @@ def main():
     parser = argparse.ArgumentParser(
         description="Chunk, embed and load text files"
     )
-    parser.add_argument("--path", help="The path of the text file to load")
+    parser.add_argument(
+        "--folder",
+        default="/code/data",
+        help="The path of the folder to load text files from. Defaults to 'data'",
+    )
     parser.add_argument(
         "--chunk-size", default=1000, help="The size of the chunks"
     )
@@ -35,16 +45,12 @@ def main():
     args = parser.parse_args()
 
     chunk_size = args.chunk_size
-    chunk_overlap = args.chunk_overlap
-    data_path = args.path
-
-    loader = TextLoader(data_path, encoding="utf8")
-    documents = loader.load()
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap
-    )
-    texts = text_splitter.split_documents(documents=documents)
+    chunk_overlap = args.overlap
+    folder = args.folder
+    if not folder or not os.path.isdir(folder):
+        raise ValueError(
+            f"Please provide a valid folder path.\n{folder} is not valid."
+        )
 
     # Get OpenAI Embeddings
     embeddings = OpenAIEmbeddings()
@@ -57,12 +63,21 @@ def main():
         embedding_function=embeddings,
         collection_name=COLLECTION_NAME,
     )
+    for filename in os.listdir(folder):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(folder, filename)
+            print(f"Processing file: {file_path}")
 
-    db.add_documents(texts)
+            loader = TextLoader(file_path, encoding="utf8")
+            documents = loader.load()
 
-    # db = PGVector.from_documents(
-    #     embedding=embeddings,
-    #     documents=texts,
-    #     collection_name=COLLECTION_NAME,
-    #     connection_string=CONNECTION_STRING,
-    # )
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=chunk_size, chunk_overlap=chunk_overlap
+            )
+            texts = text_splitter.split_documents(documents=documents)
+
+            db.add_documents(texts)
+
+
+if __name__ == "__main__":
+    main()
